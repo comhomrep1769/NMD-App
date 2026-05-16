61b52e4b00b0a3760e415bbdbf019e238942f44a
comhomrep1769 <nmdpowash@gmail.com>
2026-05-15 21:10:01 -0400

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./nmd-blue-theme.css";
+import "./components/treatments/treatment-workspace.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
