ff7ead74ac3f119ebc411cce5fca72e824ce62f3
comhomrep1769 <nmdpowash@gmail.com>
2026-05-15 20:23:43 -0400

    Create index.ts

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,5 @@
+export { default as authRoutes } from "./auth.js";
+export { default as guruRoutes } from "./guru.js";
+export { default as paymentsRoutes } from "./payments.js";
+export { default as posRoutes } from "./pos.js";
+export { default as treatmentsRoutes } from "./treatments.js";
