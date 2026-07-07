1ce4b68e7cd3772d616b78b89c8670c17c36afa0
Lobox911 <iamcarolinasmit@gmail.com>
2026-07-07 16:03:38 +0100

    Fix: avatar route types

---
+++ check-emp-imports.js
@@ -0,0 +1,6 @@
+﻿const fs = require('fs')
+const c = fs.readFileSync('backend/src/routes/employees.ts', 'utf8')
+const lines = c.split('\n')
+for (let i = 0; i < 15; i++) {
+  console.log(i + ': ' + lines[i])
+}
